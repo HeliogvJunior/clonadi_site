6d8b1a52fe065faf29c140982bc89e8449a1979c
hgvjunior <hgvjunior@gmail.com>
2015-02-14 23:38:18 -0400

    asd

--- paginas/orcamento.php
+++ paginas/orcamento.php
@@ -23,7 +23,7 @@ $assunto    = 'Orçamento Clonadi';
 $mensagem   = '<!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>TODO supply a title</title>
+        <title>Orçamento Clonadi</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
